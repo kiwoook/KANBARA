99eae2c59e99d621a4f8d9eada61ad9ff27c738b
kiwoook <tizmfns1218@Naver.com>
2024-10-01 00:34:16 +0900

    INIT

---
+++ kanbara_backend/src/main/java/com/study/kanbara/KanbaraApplication.java
@@ -0,0 +1,13 @@
+package com.study.kanbara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbaraApplication.class, args);
+    }
+
+}
---
+++ kanbara_backend/src/test/java/com/study/kanbara/KanbaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.kanbara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
